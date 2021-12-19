dcfc87f617e578ffe3ab3cd273ab5933427b54af
Brenek Harrison <brenekharrison@gmail.com>
2021-12-18 22:39:38 -0700

    Add logic to daemon run function

--- cmd/daemon/main.go
+++ cmd/daemon/main.go
@@ -12,22 +12,27 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, daemon")
-
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
 	defer cancel()
 
-	run(defaults.RebootChecker{}, defaults.BlockChecker{}, defaults.Rebooter{}, ctx)
+	run(1, defaults.RebootChecker{}, defaults.BlockChecker{}, defaults.Rebooter{}, ctx)
 }
 
-func run(rc smartautoreboot.RebootChecker, bc smartautoreboot.BlockChecker, r smartautoreboot.Rebooter, ctx context.Context) {
+func run(waitMinutes int, rc smartautoreboot.RebootChecker, bc smartautoreboot.BlockChecker, r smartautoreboot.Rebooter, ctx context.Context) {
+	fmt.Println("smartrebootd started")
+
 mainLoop:
 	for {
 		select {
 		case <-ctx.Done():
 			break mainLoop
-		case <-time.After(time.Minute):
+		case <-time.After(time.Minute * time.Duration(waitMinutes)):
 		}
 
+		if rc.IsRebootRequired() && !bc.IsRebootBlocked() {
+			if err := r.Reboot(); err != nil {
+				panic(err)
+			}
+		}
 	}
 }
