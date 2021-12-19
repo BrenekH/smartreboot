1f4fdc77eaae5f02dec1ed980505d8e1ec70b15c
Brenek Harrison <brenekharrison@gmail.com>
2021-12-18 23:16:22 -0700

    Implement defaults.RebootChecker

--- defaults/rebootchecker.go
+++ defaults/rebootchecker.go
@@ -2,6 +2,16 @@ package defaults
 
 type RebootChecker struct{}
 
+// IsRebootRequired executes all scripts in /etc/smartreboot/rebootchecks returning
+// true if at least one returned an exit code of 0, false if all were non-zero.
 func (r RebootChecker) IsRebootRequired() bool {
+	codes := runScriptsInDir("/etc/smartreboot/rebootchecks")
+
+	for _, exitCode := range codes {
+		if exitCode == 0 {
+			return true
+		}
+	}
+
 	return false
 }
