6ececcc76c1e8968e4c132d2f1199ea569d25c17
Brenek Harrison <brenekharrison@gmail.com>
2021-12-18 23:35:43 -0700

    Implement defaults.Rebooter

--- defaults/rebooter.go
+++ defaults/rebooter.go
@@ -1,7 +1,15 @@
 package defaults
 
+import "os/exec"
+
 type Rebooter struct{}
 
+// Reboot "safely" restarts the system.
 func (r Rebooter) Reboot() error {
-	return nil
+	// So this could be done to work with all linux systems by using syscall.Reboot(),
+	// but that makes us responsible for ensuring that all programs are shutdown properly
+	// (using sync(2) apparently?). Instead of dealing with that, I'd rather, at least for
+	// now, just call the shutdown command which should take care of all of that for us.
+
+	return exec.Command("shutdown", "-r", "+1").Run()
 }
