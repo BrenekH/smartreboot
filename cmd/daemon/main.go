9e54c2796efe6106ceef151843d1acfb089c0acf
Brenek Harrison <brenekharrison@gmail.com>
2021-12-18 21:57:54 -0700

    Main daemon loop

--- cmd/daemon/main.go
+++ cmd/daemon/main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
 	fmt.Println("Hello, daemon")
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
+	defer cancel()
+
+mainLoop:
+	for {
+		select {
+		case <-ctx.Done():
+			break mainLoop
+		case <-time.After(time.Minute):
+		}
+
+	}
+
+	fmt.Println("All done")
 }
